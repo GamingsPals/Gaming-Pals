aa1902f54d75c889486a3016bab5332203bf329e
Francisco Aguilar Cano <franaguilarcano@gmail.com>
2017-04-03 22:27:55 +0200

    TournamentService con confrontations y awards.

--- src/main/webapp/assets/js/app/services/TournamentService.js
+++ src/main/webapp/assets/js/app/services/TournamentService.js
@@ -1,6 +1,6 @@
 app.service("TournamentService", function(xhr){
     this.tournaments = {};
-
+    this.confrontations = {};
     this.getTournaments = function () {
         let object = this;
 
@@ -9,4 +9,18 @@ app.service("TournamentService", function(xhr){
         })
 
     }
+    
+    this.confrontations = function(tournamentId){
+        let object = this;
+        xhr.get("api/confrontations/tournament/list?tournamentId="+tournamentId ,function(response){
+            object.confrontations = response.data;
+        });
+    };
+    
+    this.awards = function(tournamentId){
+        let object = this;
+        xhr.get("api/awards/tournament/list?tournamentId="+tournamentId ,function(response){
+            object.awards = response.data;
+        });
+    };
 });
